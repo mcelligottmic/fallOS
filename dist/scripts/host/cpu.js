614cacb48c20f4fb4a8bcc4d2e4b7916dfb9ff52
Michael McElligott <michael.mcelligott2@Marist.edu>
2014-10-29 23:58:52 -0400

    more functions to CPU

--- dist/scripts/host/cpu.js
+++ dist/scripts/host/cpu.js
@@ -14,11 +14,11 @@ var TSOS;
 (function (TSOS) {
     var Cpu = (function () {
         function Cpu(PC, Acc, Xreg, Yreg, Zflag, isExecuting) {
-            if (typeof PC === "undefined") { PC = 0; }
-            if (typeof Acc === "undefined") { Acc = 0; }
-            if (typeof Xreg === "undefined") { Xreg = 0; }
-            if (typeof Yreg === "undefined") { Yreg = 0; }
-            if (typeof Zflag === "undefined") { Zflag = 0; }
+            if (typeof PC === "undefined") { PC = "00"; }
+            if (typeof Acc === "undefined") { Acc = "00"; }
+            if (typeof Xreg === "undefined") { Xreg = "00"; }
+            if (typeof Yreg === "undefined") { Yreg = "00"; }
+            if (typeof Zflag === "undefined") { Zflag = "00"; }
             if (typeof isExecuting === "undefined") { isExecuting = false; }
             this.PC = PC;
             this.Acc = Acc;
@@ -28,18 +28,113 @@ var TSOS;
             this.isExecuting = isExecuting;
         }
         Cpu.prototype.init = function () {
-            this.PC = 0;
-            this.Acc = 0;
-            this.Xreg = 0;
-            this.Yreg = 0;
-            this.Zflag = 0;
-            this.isExecuting = false;
+            this.PC = "00", this.Acc = "00", this.Xreg = "00", this.Yreg = "00", this.Zflag = "00", this.isExecuting = false;
         };
 
         Cpu.prototype.cycle = function () {
             _Kernel.krnTrace('CPU cycle');
             // TODO: Accumulate CPU usage and profiling statistics here.
             // Do the real work here. Be sure to set this.isExecuting appropriately.
+        };
+
+        //convert hex to memory location
+        Cpu.prototype.memoryToInt = function (first, second) {
+            return parseInt((second + first), 16);
+        };
+
+        //get element from memory given an address
+        Cpu.prototype.dataAt = function (location) {
+            //check to see if we are out of bounds with memory access
+            return _MemoryManager.memory.RAM[location];
+        };
+
+        //store element to memory given an address and an element
+        Cpu.prototype.storeAt = function (loaction, data) {
+            //check to see if we have access
+            _MemoryManager.memory.RAM[location] = data;
+        };
+
+        //Increase the PC by a set amount
+        Cpu.prototype.addToPC = function (num) {
+            this.PC = (parseInt(this.PC, 16) + num).toString(16);
+        };
+
+        //LDA A9
+        //Load the accumulator with a constant
+        Cpu.prototype.LDAc = function () {
+            var constant = this.dataAt(parseInt(this.PC, 16) + 1);
+            this.Acc = constant;
+            this.addToPC(3);
+        };
+
+        //LDA AD
+        //Load the accumulator from memory
+        Cpu.prototype.LDAm = function () {
+            var address = this.memoryToInt(this.dataAt(parseInt(this.PC, 16) + 1), this.dataAt(parseInt(this.PC, 16) + 2));
+            var constant = this.dataAt(address);
+            this.Acc = constant;
+            this.addToPC(3);
+        };
+
+        //STA 8D
+        //Store the accumulator in memory
+        Cpu.prototype.STAm = function () {
+            var address = this.memoryToInt(this.dataAt(parseInt(this.PC, 16) + 1), this.dataAt(parseInt(this.PC, 16) + 2));
+
+            //store in memory
+            this.storeAt(address, this.Acc);
+            this.addToPC(3);
+        };
+
+        //ADC 6D
+        //Add with carry
+        //Adds contents of an address to the contents of the accumulator and keeps the result in the accumulator
+        Cpu.prototype.ADC = function () {
+            var value = this.memoryToInt(this.dataAt(parseInt(this.PC, 16) + 1), this.dataAt(parseInt(this.PC, 16) + 2));
+            this.Acc = (parseInt(this.Acc, 16) + value).toString(16);
+            this.addToPC(3);
+        };
+
+        //LDX A2
+        //Load the X register with a constant
+        Cpu.prototype.LDXc = function () {
+            this.Xreg = this.dataAt(parseInt(this.PC, 16) + 1);
+            this.addToPC(2);
+        };
+
+        //LDX AE
+        //Load the X register from memory
+        Cpu.prototype.LDXm = function () {
+            this.Xreg = (this.memoryToInt(this.dataAt(parseInt(this.PC, 16) + 1), this.dataAt(parseInt(this.PC, 16) + 2))).toString(16);
+            this.addToPC(3);
+        };
+
+        //LDY A0
+        //Load the Y register from a constant
+        Cpu.prototype.LDYc = function () {
+            this.Yreg = this.dataAt(parseInt(this.PC, 16) + 1);
+            this.addToPC(2);
+        };
+
+        //LDY AC
+        //Load the Y register from memory
+        Cpu.prototype.LDYm = function () {
+            this.Yreg = (this.memoryToInt(this.dataAt(parseInt(this.PC, 16) + 1), this.dataAt(parseInt(this.PC, 16) + 2))).toString(16);
+            this.addToPC(3);
+        };
+
+        //NOP EA
+        //No Operation
+        Cpu.prototype.NOP = function () {
+            //...I know a guy who would be upset with the waste cycle
+            this.addToPC(1);
+        };
+
+        //BRK 00
+        //Break (which is really a system call)
+        Cpu.prototype.BRK = function () {
+            //figure out what to do here <----
+            this.addToPC(1);
         };
         return Cpu;
     })();
--- source/scripts/host/cpu.ts
+++ source/scripts/host/cpu.ts
@@ -19,21 +19,21 @@ module TSOS {
 
     export class Cpu {
 
-        constructor(public PC: number = 0,
-                    public Acc: number = 0,
-                    public Xreg: number = 0,
-                    public Yreg: number = 0,
-                    public Zflag: number = 0,
+        constructor(public PC: string = "00",
+                    public Acc: string = "00",
+                    public Xreg: string = "00",
+                    public Yreg: string = "00",
+                    public Zflag: string = "00",
                     public isExecuting: boolean = false) {
 
         }
 
         public init(): void {
-            this.PC = 0;
-            this.Acc = 0;
-            this.Xreg = 0;
-            this.Yreg = 0;
-            this.Zflag = 0;
+            this.PC = "00",
+            this.Acc = "00",
+            this.Xreg = "00",
+            this.Yreg = "00",
+            this.Zflag = "00",
             this.isExecuting = false;
         }
 
@@ -42,5 +42,121 @@ module TSOS {
             // TODO: Accumulate CPU usage and profiling statistics here.
             // Do the real work here. Be sure to set this.isExecuting appropriately.
         }
+
+        //convert hex to memory location
+        public memoryToInt(first: string, second: string): number {
+          return parseInt((second + first), 16);
+        }
+
+        //get element from memory given an address
+        public dataAt(location: number): string{
+          //check to see if we are out of bounds with memory access
+          return _MemoryManager.memory.RAM[location];
+        }
+
+        //store element to memory given an address and an element
+        public storeAt(loaction: number, data: string): void {
+          //check to see if we have access
+          _MemoryManager.memory.RAM[location] = data;
+        }
+
+        //Increase the PC by a set amount
+        public addToPC(num: number): void {
+          this.PC = (parseInt(this.PC, 16) + num).toString(16);
+        }
+
+        //LDA A9
+        //Load the accumulator with a constant
+        public LDAc(): void {
+          var constant = this.dataAt(parseInt(this.PC, 16) + 1);
+          this.Acc = constant;
+          this.addToPC(3);
+        }
+
+        //LDA AD
+        //Load the accumulator from memory
+        public LDAm(): void {
+          var address = this.memoryToInt(this.dataAt(parseInt(this.PC, 16) + 1),
+                        this.dataAt(parseInt(this.PC, 16) + 2));
+          var constant = this.dataAt(address);
+          this.Acc = constant;
+          this.addToPC(3);
+        }
+
+        //STA 8D
+        //Store the accumulator in memory
+        public STAm(): void {
+          var address = this.memoryToInt(this.dataAt(parseInt(this.PC, 16) + 1),
+                        this.dataAt(parseInt(this.PC, 16) + 2));
+          //store in memory
+          this.storeAt(address, this.Acc);
+          this.addToPC(3);
+        }
+
+        //ADC 6D
+        //Add with carry
+        //Adds contents of an address to the contents of the accumulator and keeps the result in the accumulator
+        public ADC(): void {
+          var value = this.memoryToInt(this.dataAt(parseInt(this.PC, 16) + 1),
+                      this.dataAt(parseInt(this.PC, 16) + 2));
+          this.Acc = (parseInt(this.Acc, 16) + value).toString(16);
+          this.addToPC(3);
+        }
+
+        //LDX A2
+        //Load the X register with a constant
+        public LDXc(): void {
+          this.Xreg = this.dataAt(parseInt(this.PC, 16) + 1);
+          this.addToPC(2);
+        }
+
+        //LDX AE
+        //Load the X register from memory
+        public LDXm(): void {
+          this.Xreg = (this.memoryToInt(this.dataAt(parseInt(this.PC, 16) + 1),
+                      this.dataAt(parseInt(this.PC, 16) + 2))).toString(16);
+          this.addToPC(3);
+        }
+
+        //LDY A0
+        //Load the Y register from a constant
+        public LDYc(): void {
+          this.Yreg = this.dataAt(parseInt(this.PC, 16) + 1);
+          this.addToPC(2);
+        }
+
+        //LDY AC
+        //Load the Y register from memory
+        public LDYm(): void {
+          this.Yreg = (this.memoryToInt(this.dataAt(parseInt(this.PC, 16) + 1),
+                      this.dataAt(parseInt(this.PC, 16) + 2))).toString(16);
+          this.addToPC(3);
+        }
+
+        //NOP EA
+        //No Operation
+        public NOP(): void {
+          //...I know a guy who would be upset with the waste cycle
+          this.addToPC(1);
+        }
+
+        //BRK 00
+        //Break (which is really a system call)
+        public BRK(): void {
+          //figure out what to do here <----
+          this.addToPC(1);
+        }
+
+        //CPX EC
+        //Compare a byte in memory to the X-reg sets the Z(zero) flag if equal
+
+        //BNE D0
+        //Branch n bytes if Z flag = 0
+
+        //INC EE
+        //Increment the value of a byte
+
+        //SYS FF
+        //System Call
     }
 }
